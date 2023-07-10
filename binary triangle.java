2b14219e3c9d561eb189705b6e2196bca87bc814
harshtyagi0908 <139032142+harshtyagi0908@users.noreply.github.com>
2023-07-10 11:17:06 +0530

    Update and rename Number  pyramid to binary triangle.java

---
+++ binary triangle.java
@@ -0,0 +1,25 @@
+0
+10
+010
+1010
+01010
+
+int n=5;
+        
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                 int sum=i+j;
+                 if(sum%2==0){
+                     System.out.print("0");
+                 }
+                 else{
+                     System.out.print("1");
+                 }
+                 
+            }
+System.out.println();
+}
+}
+
+
+
