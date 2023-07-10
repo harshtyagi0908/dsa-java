a41e5bd18d07dd60abff24227cbdb6fa9c7f8e79
harshtyagi0908 <139032142+harshtyagi0908@users.noreply.github.com>
2023-07-10 11:39:49 +0530

    Create fibonacci.java

---
+++ loops/fibonacci.java
@@ -0,0 +1,18 @@
+int n=5;
+	  int i=1;
+	  
+	  int a=0;
+	  int b=1;
+	 while(i<=n) {
+		 int c=a+b;
+		 System.out.println(c);
+	 
+	 a=b;
+	 b=c;
+	 i++;
+	 }
+	 System.out.println(a);
+			  
+	   
+		
+	}
