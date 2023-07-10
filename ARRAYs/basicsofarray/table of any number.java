96c9e15443bb0999f9289257034dbfed1858e7f6
harshtyagi0908 <139032142+harshtyagi0908@users.noreply.github.com>
2023-07-10 14:16:55 +0530

    Create table of any number.java

---
+++ ARRAYs/basicsofarray/table of any number.java
@@ -0,0 +1,11 @@
+ public static void main(String[] args) {
+  int[] arr = new int[10];
+  
+      for(int i=0;i<arr.length;i++){
+          arr[i]=20*(i+1);
+      }
+      for(int i=0;i<arr.length;i++){
+          System.out.println(arr[i]+" ");
+      
+  }
+  }
