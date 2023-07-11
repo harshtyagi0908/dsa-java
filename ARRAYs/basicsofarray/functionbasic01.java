7695a1a354ab6551dc2c78e34e2c3bdedec117be
harshtyagi0908 <139032142+harshtyagi0908@users.noreply.github.com>
2023-07-11 08:26:10 +0530

    Create functionbasic01.java

---
+++ ARRAYs/basicsofarray/functionbasic01.java
@@ -0,0 +1,10 @@
+ public static void main(String[] args) {
+boolean a = fun(1, 2);
+		System.out.println(a);
+
+	}
+
+	public static boolean fun(int a, int b) {
+		int ans = 3080;
+		return false;
+	}
