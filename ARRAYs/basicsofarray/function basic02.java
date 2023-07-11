3f2fe881d93861172463839b49ccae5b5827673d
harshtyagi0908 <139032142+harshtyagi0908@users.noreply.github.com>
2023-07-11 08:32:16 +0530

    Create function basic02.java

---
+++ ARRAYs/basicsofarray/function basic02.java
@@ -0,0 +1,27 @@
+public static void main(String[] args) {
+System.out.println("hello!");
+		fun1();
+		System.out.println(" Good Bye!");
+	}
+
+	public static void fun1() {
+		System.out.println(" start fun 1:");
+		fun2();
+		System.out.println(" end");
+	}
+
+	public static void fun2() {
+		System.out.println(" start func 2:");
+		fun3();
+		System.out.println(" end");
+	}
+
+	public static void fun3() {
+		System.out.println(" start fun 3:");
+		System.out.println(" end");
+		
+	}
+}
+
+
+    
