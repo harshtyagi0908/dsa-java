825db7c4f0accd23dcd2d167177c5b00be682e3e
harshtyagi0908 <139032142+harshtyagi0908@users.noreply.github.com>
2023-07-12 10:03:36 +0530

    Create substring basic.java

---
+++ string/substring basic.java
@@ -0,0 +1,5 @@
+ public static void main(String[] args) {
+       String sentence="My name is tommy vercetti";
+       String name=sentence.substring(11,sentence.length());
+       System.out.println(name);
+    }
