b8911589a878b01710c7658280ed1ba4a9a3fa06
harshtyagi0908 <139032142+harshtyagi0908@users.noreply.github.com>
2023-07-12 10:43:14 +0530

    Create string reverse.java

---
+++ string/string reverse.java
@@ -0,0 +1,13 @@
+  public static void main(String[] args) {
+    StringBuilder sb= new StringBuilder("sky is blue");
+       for(int i=0;i<sb.length()/2;i++){
+           int front=i;
+           int back=sb.length()-1-i;
+           
+           char frontChar=sb.charAt(front);
+           char backChar=sb.charAt(back);
+           sb.setCharAt(front,backChar);
+           sb.setCharAt(back,frontChar);
+       }
+    System.out.println(sb);
+    }
