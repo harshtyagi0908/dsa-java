8cf564c606a6fa8f7fb270dbfbc8df9e0258963b
harshtyagi0908 <139032142+harshtyagi0908@users.noreply.github.com>
2023-07-13 08:18:10 +0530

    Create sum of n natural number.java

---
+++ Recursion/sum of n natural number.java
@@ -0,0 +1,16 @@
+public static void printsum(int i,int n,int sum){
+       if(i==n){
+           sum=sum+i;
+           System.out.println(sum);
+           return;
+       }
+       sum=sum+i;
+       printsum(i+1,n,sum);
+    }    
+       
+       public static void main(String []args){
+         
+           printsum(1,8,0);
+       }
+
+}
